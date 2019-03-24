4f817afcd0a8728ea9955b0a89f818ffba02f5b9
wiemrh <ouiemm.je@gmail.com>
2019-03-24 14:21:51 +0100

    update

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Accueil from './screens/Accueil'
+import Login from './screens/Login'
+import Profil from './screens/Profil'
+
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+export default class App extends React.Component {
+  render() {
+    return (
+     <MyApp />
+    );
+  }
+}
+
+const MainNavigator = createStackNavigator({
+
+  Login: {screen: Login},
+  Accueil: {screen: Accueil},
+  Profil: {screen: Profil},
+
+
+},
+
+{
+  mode: 'modal',
+  headerMode: 'none',
+}
+);
+
+const MyApp = createAppContainer(MainNavigator);
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
